edcc035cdd4e651eaf09d0095af39ee42e968669
mk360 <36678834+mk360@users.noreply.github.com>
2019-04-25 22:07:24 +0300

    MAKE THE CODE WORK

---
+++ src/database.js
@@ -0,0 +1,18 @@
+const {MongoClient} = require("mongodb");
+const auth = require("./auth.json");
+
+if (!auth.dbUrl) {
+	throw Error("Missing database credentials. Exiting process.");
+	process.exit(1)
+}
+
+const client = new MongoClient(auth.dbUrl, { useNewUrlParser: true});
+
+client.connect(function(err) {
+	if (err) {
+		throw err;
+	}
+	console.log("Database server connected and ready to operate.");
+});
+
+module.exports = client;
