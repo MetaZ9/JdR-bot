74086537383d6624eec5451a00fbef75c00d0aad
MetaZ9 <32413721+MetaZ9@users.noreply.github.com>
2019-04-25 18:53:11 +0200

    Create rule.js

---
+++ src/rule.js
@@ -0,0 +1,51 @@
+const grades = require('grades.js')
+const mongodb = require('mongodb.js')
+
+class Rule
+{
+	let ruleName = ''
+	let isHidden = true
+	let minGrade = Grades.ADMIN
+	let ruleCore = {}
+	let callback = ''
+
+	function createRule()
+	{
+		//create logic
+
+		setRuleByName(name);
+	}
+
+	function setRuleByName(name)
+	{
+		//export to database
+
+		return this
+	}
+
+	function getRuleByName(name)
+	{
+		//import from database
+
+		return this
+	}
+
+	function showRuleByName(name)
+	{
+		//show logic
+
+	}
+
+	function alterRuleByName(name)
+	{
+		//alter logic
+
+		return this
+	}
+
+	function deleteRuleByName(name)
+	{
+		//deleteRule
+	}
+
+}
