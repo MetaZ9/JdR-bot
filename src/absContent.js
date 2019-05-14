36c59b01b3c5c104ddc1729a487538dcdd551f46
MetaZ9 <32413721+MetaZ9@users.noreply.github.com>
2019-05-14 20:11:12 +0200

    Ajout de l'héritage

---
+++ src/absContent.js
@@ -0,0 +1,100 @@
+const Database = require('./database.js');
+const auth = require('./auth.json');
+const {compact, isObject} = require('lodash');
+
+class AbstractContent {
+	constuctor () {
+
+	};
+
+};
+
+AbstractContent.prototype.create = function(content, collection) {
+	this.validate(content);
+	return this.set(content, collection);
+};
+
+AbstractContent.prototype.alter = function(name, newContent, collectionName, resolveNewContent = true) {
+	this.validate(newContent);
+	return new Promise((resolve, reject) => {
+		let newObject = this.format(newContent);
+		newObject._id = name;
+
+		Database.db(auth.dbName).collection(collectionName).findOneAndReplace({_id: name}, newObject, {"returnNewDocument" : resolveNewContent}
+		).then((error, content) => {
+			if (error) {
+				throw error;
+			}
+
+			resolve(content);
+			// https://docs.mongodb.com/manual/reference/method/db.getCollection.findAndModify/#db.getCollection.findAndModify
+			// applying "true" to the "new" parameter forces mongodb to return the updated document
+		});
+	});
+};
+
+AbstractContent.prototype.get = function(name, collectionName) {
+	return new Promise((resolve, reject) => {
+		if (name === this.name) {
+			resolve({
+				//assign values ?
+				_id: this.ruleName,
+				isHidden: this.isHidden,
+				minGrade: this.minGrade,
+				ruleCore: this.ruleCore,
+				callback: this.callback
+			});
+		} else {
+			Database.db(auth.dbName).collection(collectionName).findOne({_id: name}).then((error, content) => {
+				if (error) {
+					throw error;
+				}
+
+				this.cache(content);
+				resolve(content);
+			});
+		}
+	});
+};
+
+//J'ai juste fait ça pour que ça marche, faudra complètement la repenser x)
+//Est-ce qu'on ferait pas un query manager ? Je pense que ça sera le mieux
+AbstractContent.prototype.getAll = function(params, collectionName) {
+	return new Promise((resolve, reject) => {
+		Database.db(auth.dbName).collection(collectionName).find(params).then((error, contents) => {
+			if (error) {
+				throw error;
+			}
+
+			//this.cacheRule(rule);
+			resolve(contents);
+		});
+
+	});
+
+};
+
+AbstractContent.prototype.set = function(content, collectionName) {
+	const toInsert = this.format(content);
+	return new Promise((resolve, reject) => {
+		Database.db(auth.dbName).collection(collectionName).insertOne(toInsert).then((error, inserted) => {
+			resolve(inserted);
+		});
+	});
+};
+
+AbstractContent.prototype.delete = function(name, collectionName) {
+	return new Promise((resolve, reject) => {
+		Database.db(auth.dbName).collection(collectionName).deleteOne({_id: name}).then(deleted => {
+			if (error) {
+				throw error;
+			}
+			resolve(Boolean(deleted.deletedCount));
+			// use this as a signal on whether
+			// the rule existed and was deleted (returns true)
+			// or it didn't exist at the first place (returns false)
+		});
+	});
+};
+
+module.exports = AbstractContent;
--- src/rule.js
+++ src/rule.js
@@ -2,17 +2,99 @@ const Grades = require('./grades.js');
 const Database = require('./database.js');
 const auth = require('./auth.json');
 const {compact, isObject} = require('lodash');
+const AbstractContent = require('./absContent.js')
+const collectionName = auth.dbCollections.rules;
 
-class Rule {
+class Rule extends AbstractContent {
 	constructor() {
+		super();
 		this.ruleName = '';
 		this.isHidden = true;
 		this.minGrade = Grades.ADMIN;
 		this.ruleCore = {};
 		this.callback = '';
 	};
+
 };
 
+Rule.prototype.createRule = function (rule) {
+	return this.create(rule, collectionName);
+}
+
+Rule.prototype.alterRule = function (name, newRule, resolveNewRule = true) {
+	return this.alter(name, newRule, collectionName, resolveNewRule);
+};
+
+Rule.prototype.getRule = function (name) {
+	return this.get(name, collectionName);
+}
+
+Rule.prototype.getAllRules = function (params) {
+	return this.getAllRules(params, collectionName);
+}
+
+Rule.prototype.setRule = function (newRule) {
+	return this.setRule(newRule, collectionName);
+}
+
+Rule.prototype.deleteRule = function (name) {
+	return this.delete(name, collectionName)
+}
+
+Rule.prototype.cache = function(rule) {
+	this.ruleName = rule._id;
+	this.isHidden = rule.isHidden;
+	this.minGrade = rule.minGrade;
+	this.ruleCore = rule.ruleCore;
+	this.callback = rule.callback;
+};
+Rule.prototype.cacheRule = function (rule) {
+	return this.cache(rule);
+};
+
+Rule.prototype.validate = function(rule) {
+	if (typeof rule.ruleName !== "string") {
+		throw Error("Rule name must be a string.");
+	}
+
+	if (typeof rule.isHidden !== "boolean") {
+		throw Error("Whether a rule is hidden or not must be a boolean."); // could be a bit shorter
+	}
+
+	if (isNaN(rule.minGrade)) {
+		throw Error("Minimal Grade must be an integer.");
+	}
+
+	if (!isObject(rule.ruleCore)) {
+		throw Error("Rule core must be an object.");
+	}
+
+	if (rule.callback) {
+		if (typeof rule.callback !== "string") {
+			throw Error("Callback must be a string.");
+		}
+		else if (rule.callback === rule.ruleName) {
+			throw Error("Callback references itself");
+		}
+
+	}
+
+};
+Rule.prototype.validateRule = function (rule) {
+	return this.validate(rule);
+}
+
+Rule.prototype.format = function(rule) {
+	let copy = Object.assign({}, rule);
+	copy._id = copy.ruleName;
+	delete copy.ruleName;
+	return copy;
+};
+Rule.prototype.formatRule = function (rule) {
+	return this.format(rule);
+}
+
+/*
 Rule.prototype.alterRule = function(name, newRule, resolveNewRule = true) {
 	this.validateRule(newRule);
 	return new Promise((resolve, reject) => {
@@ -135,5 +217,5 @@ Rule.prototype.validateRule = function(rule) {
 		throw Error("Callback must be a string.");
 	}
 };
-
+*/
 module.exports = new Rule();
