01cce2edfc712a97553fb6c278e5986ec7fd5a02
mk360 <36678834+mk360@users.noreply.github.com>
2019-05-16 05:22:45 +0300

    complete content test suite

--- test/content.js
+++ test/content.js
@@ -17,6 +17,17 @@ describe("Content validator", () => {
 	let goodContent = {
 		name: "nik"
 	};
+	let altContent = {
+		name: "bruh"
+	};
+
+	it("should properly format content", () => {
+		let {name} = goodContent;
+		let newContent = Content.formatContent(goodContent);
+		expect(newContent.name).toBe(undefined);
+		expect(newContent._id).toBe(name);
+	});
+
 	it("should properly validate content", () => {
 		expect(function() {
 			Content.validate(messedContent);
@@ -33,5 +44,29 @@ describe("Content validator", () => {
 		expect(function() {
 			Content.validate(goodContent);
 		}).not.toThrow();
+	});
+
+	it("should resolve all promises when connected to DB", () => {
+		Client.connect(() => {
+			expectAsync(Content.setContent(goodContent)).toResolve();
+			expectAsync(Content.getContent(goodContent)).toResolve();
+			expectAsync(Content.alterContent(goodContent.name, altContent)).toResolve();
+			expectAsync(Content.deleteContent(altContent)).toBe(true); // content found and deleted
+			expectAsync(Content.deleteContent(altContent)).toBe(false); // deleted once, doesn't exist anymore
+			Client.close();
+		});
+	});
+
+	it("should reject all promises when not connected to DB", () => {
+		expectAsync(Content.setContent(goodContent)).toBeRejected();
+		expectAsync(Content.setContent(goodContent)).toBeRejected();
+		expectAsync(Content.getContent(goodContent)).toBeRejected();
+		expectAsync(Content.alterContent(goodContent.name, altContent)).toBeRejected();
+		expectAsync(Content.deleteContent(goodContent)).toBeRejected();
+	});
+
+	it("should cache content properly", () => {
+		Content.cache(Content.format(goodContent));
+		expect(Content.ruleDefinedVars).toEqual(goodContent);
 	});
 });
