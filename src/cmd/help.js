23ca9f99b6d9e490be4499520c9651c5a40f1d71
MetaZ9 <32413721+MetaZ9@users.noreply.github.com>
2019-07-09 19:33:17 +0200

    Minor correction

--- src/cmd/help.js
+++ src/cmd/help.js
@@ -3,24 +3,19 @@ const helpMessageList = require('help.json');
 var helpMessages = ['Look ! An error'];		// Au cas où les deux fonctions foirent, ça te permet d'avoir un cas d'erreur
 											// Je la mets en global pour éviter d'avoir à la passer en paramètre
 
-function sendHelp(command)
-{
+function sendHelp(command) {
 	helpingString = command ? prepareDetailedHelp(command) : prepareCommandList();
 	sendHelpMessage(helpingString);
 	//Est-ce que je pousse le vice jusqu'au bout en faisant sendHelpMessage(command ? prepareDetailedHelp(command) : prepareCommandList()); :} ?
 }
 
-function prepareCommandList()
-{
+function prepareCommandList() {
 	for (var helpCommand in helpMessageList)
-	{
 		helpMessages.push({name: helpCommand.name, desc: helpCommand.shortDesc});
-	}
-
 }
 
-function prepareDetailedHelp(command)
-{
+
+function prepareDetailedHelp(command) {
 	if (!command)
 		return;						//should not be here => error | Pas nécessaire, juste pour test au début
 
